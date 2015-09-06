8967aced101d3bd04daa71bf5c9cb836ad204f49
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-06 19:29:04 -0400

    view/collection creating working

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ var Base = require('base-methods');
 var router = require('en-route');
 var extend = require('extend-shallow');
 var define = require('define-property');
-var delegate = require('delegate-properties');
-var layouts = require('layouts');
 var utils = require('./lib/utils')(require);
 var Views = require('./lib/views');
+var List = require('./lib/list');
 var View = require('./lib/view');
+var lib = require('./lib/');
+
 
 function Templates(options) {
   if (!(this instanceof Templates)) {
@@ -23,11 +24,7 @@ function Templates(options) {
   }
   Base.call(this);
   this.options = options || {};
-  this.define('Views', this.options.Views || Views);
-  this.define('View', this.options.View || View);
-  delete this.options.Views;
-  delete this.options.View;
-  this.views = {};
+  this.initialize();
 }
 
 Base.extend(Templates);
@@ -36,21 +33,261 @@ Base.extend(Templates);
  * `Templates` prototype methods
  */
 
-delegate(Templates.prototype, {
+utils.delegate(Templates.prototype, {
   constructor: Templates,
 
-  create: function(name, options) {
-    var views = !(options instanceof this.Views)
-      ? new this.Views(options)
-      : options;
+  /**
+   * Initialize defaults
+   */
 
-    define(this, name, views.addView);
-    define(this.views, name, views.views);
+  initialize: function () {
+    this.engines = {};
+    this.define('_', {});
+    this._.engines = new utils.Engines(this.engines);
 
-    for (var key in views) {
-      define(this[name], key, views);
-    }
+    this.listen();
+    // lib.helpers.methods(this);
+    // lib.lookup(this);
+
+    // temporary.
+    this.define('errors', {
+      compile: {
+        engine: 'cannot find an engine for: ',
+        method: 'expects engines to have a compile method',
+      },
+      render: {
+        callback: 'is async and expects a callback function: ',
+        engine: 'cannot find an engine for: ',
+        method: 'expects engines to have a render method',
+      }
+    });
+
+    this.cache = {};
+    this.cache.data = {};
+    this.cache.context = {};
+
+    this.items = {};
+    this.views = {};
+
+    this.define('Base', require('base-methods'));
+    this.define('View', this.options.View || View);
+    this.define('List', this.options.List || List);
+    this.define('Views', this.options.Views || Views);
+
+    this.viewTypes = {
+      layout: [],
+      renderable: [],
+      partial: []
+    };
+
+    this.inflections = {};
+    this.handlers(utils.methods);
+  },
+
+  /**
+   * Listen for events
+   */
+
+  listen: function () {
+    this.on('error', function (msg, err) {
+      console.error(msg.message, err);
+    });
+
+    this.on('option', function (key, value) {
+      if (key === 'mixins') {
+        this.visit('mixin', value);
+      }
+    });
+  },
+
+  /**
+   * Create a new `Views` collection.
+   *
+   * ```js
+   * app.create('foo');
+   * app.foo('*.hbs');
+   * var view = app.foo.get('baz.hbs');
+   * ```
+   *
+   * @name .create
+   * @param  {String} `name` The name of the collection. Plural or singular form.
+   * @param  {Object} `opts` Collection options
+   * @param  {String|Array|Function} `loaders` Loaders to use for adding views to the created collection.
+   * @return {Object} Returns the `Template` instance for chaining.
+   * @api public
+   */
+
+  create: function(name, opts) {
+    var collection = !(opts instanceof this.Views)
+      ? new this.Views(opts)
+      : opts;
+
+    // get the collection inflections, e.g. page/pages
+    var single = utils.single(name);
+    var plural = utils.plural(name);
+
+    // map the inflections for lookups
+    this.inflections[single] = plural;
+
+    // set defaults on the collection options
+    opts = utils.merge({ renameKey: this.options.renameKey }, opts);
+
+    // add the collection to `app.views`
+    define(this.views, plural, collection.views);
+
+    // create loader functions for adding views to this collection
+    define(this, plural, collection.addViews.bind(collection));
+    define(this, single, collection.addView.bind(collection));
+
+    // decorate loader methods with collection methods
+    utils.forward(this[plural], collection);
+    utils.forward(this[single], collection);
+
+    // create aliases on the collection for
+    // addView/addViews to support chaining
+    collection.set(plural, this[plural]);
+    collection.set(single, this[single]);
     return this;
+  },
+
+  /**
+   * Returns the first template from the given collection with a key
+   * that matches the given glob pattern.
+   *
+   * ```js
+   * var pages = app.matchView('pages', 'home.*');
+   * //=> {'home.hbs': { ... }, ...}
+   *
+   * var posts = app.matchView('posts', '2010-*');
+   * //=> {'2015-10-10.md': { ... }, ...}
+   * ```
+   *
+   * @param {String} `collection` Collection name.
+   * @param {String} `pattern` glob pattern
+   * @param {Object} `options` options to pass to [micromatch]
+   * @return {Object}
+   * @api public
+   */
+
+  matchView: function(collection, pattern, options) {
+    var views = this.getViews(collection);
+    if (views.hasOwnProperty(pattern)) {
+      return views[pattern];
+    }
+    return utils.matchKey(views, pattern, options);
+  },
+
+  /**
+   * Returns any templates from the specified collection with keys
+   * that match the given glob pattern.
+   *
+   * ```js
+   * var pages = app.matchViews('pages', 'home.*');
+   * //=> {'home.hbs': { ... }, ...}
+   *
+   * var posts = app.matchViews('posts', '2010-*');
+   * //=> {'2015-10-10.md': { ... }, ...}
+   * ```
+   *
+   * @param {String} `collection` Collection name.
+   * @param {String} `pattern` glob pattern
+   * @param {Object} `options` options to pass to [micromatch]
+   * @return {Object}
+   * @api public
+   */
+
+  matchViews: function(collection, pattern, options) {
+    var views = this.getViews(collection);
+    return utils.matchKeys(views, pattern, options);
+  },
+
+  /**
+   * Get a specific template from the specified collection.
+   *
+   * ```js
+   * app.getView('pages', 'a.hbs', function(fp) {
+   *   return path.basename(fp);
+   * });
+   * ```
+   *
+   * @param {String} `collectionName` Collection name, like `pages`
+   * @param {String} `key` Template name
+   * @param {Function} `fn` Optionally pass a `renameKey` function
+   * @return {Object}
+   * @api public
+   */
+
+  getView: function(collection, key, fn) {
+    var views = this.getViews(collection);
+    // if a custom renameKey function is passed, try using it
+    if (typeof fn === 'function') {
+      key = fn(key);
+    }
+    if (views.hasOwnProperty(key)) {
+      return views[key];
+    }
+    // try again with the default renameKey function
+    fn = this.option('renameKey');
+    var name;
+    if (typeof fn === 'function') {
+      name = fn(key);
+    }
+    if (name && name !== key && views.hasOwnProperty(name)) {
+      return views[name];
+    }
+    return null;
+  },
+
+  /**
+   * Get a view `collection` by its singular or plural name.
+   *
+   * ```js
+   * var pages = app.getViews('pages');
+   * //=> { pages: {'home.hbs': { ... }}
+   *
+   * var posts = app.getViews('posts');
+   * //=> { posts: {'2015-10-10.md': { ... }}
+   * ```
+   *
+   * @param {String} `plural` The plural collection name, e.g. `pages`
+   * @return {Object}
+   * @api public
+   */
+
+  getViews: function(plural) {
+    if (utils.isObject(plural)) return plural;
+    if (!this.views.hasOwnProperty(plural)) {
+      plural = this.inflections[plural];
+    }
+    if (!this.views.hasOwnProperty(plural)) {
+      throw new Error('getViews cannot find collection' + plural);
+    }
+    return this.views[plural];
+  },
+
+  /**
+   * Get a view by `name` from the given `collection`.
+   */
+
+  lookup: function (name, collection) {
+    if (typeof name !== 'string') {
+      return null;
+    }
+
+    if (typeof collection === 'string') {
+      return this[collection].get(name);
+    }
+
+    var collections = this.viewTypes.renderable;
+    var len = collections.length, i = 0;
+    while (len--) {
+      var plural = collections[i++];
+      var views = this.views[plural];
+      var res;
+      if (res = views[name]) {
+        return res;
+      }
+    }
   },
 
   /**
@@ -177,6 +414,53 @@ delegate(Templates.prototype, {
   },
 
   /**
+   * Register a view engine callback `fn` as `ext`.
+   *
+   * @param {String|Array} `exts` One or more file extensions.
+   * @param {Function|Object} `fn` or `settings`
+   * @param {Object} `settings`
+   * @api public
+   */
+
+  engine: function(exts, fn, settings) {
+    if (arguments.length === 1 && typeof exts === 'string') {
+      return this.getEngine(exts);
+    }
+
+    if (!Array.isArray(exts) && typeof exts !== 'string') {
+      throw new TypeError('expected engine ext to be a string or array.');
+    }
+    utils.arrayify(exts).forEach(function (ext) {
+      this.setEngine(ext, fn, settings);
+    }.bind(this));
+    return this;
+  },
+
+  /**
+   * Register an engine for `ext` with the given `settings`
+   *
+   * @param {String} `ext` The engine to get.
+   * @api public
+   */
+
+  setEngine: function(ext, fn, settings) {
+    this._.engines.setEngine(utils.formatExt(ext), fn, settings);
+    return this;
+  },
+
+  /**
+   * Get the engine settings registered for the given `ext`.
+   *
+   * @param {String} `ext` The engine to get.
+   * @api public
+   */
+
+  getEngine: function(ext) {
+    ext = utils.formatExt(ext || this.option('view engine'));
+    return this._.engines.getEngine(ext);
+  },
+
+  /**
    * Apply a layout to the given `view`.
    *
    * @name .applyLayout
@@ -225,7 +509,7 @@ delegate(Templates.prototype, {
     extend(opts, view.context());
 
     // actually apply the layout
-    var res = layouts(str, name, stack, opts, handleLayout);
+    var res = utils.layouts(str, name, stack, opts, handleLayout);
 
     view.option('layoutStack', res.history);
     view.option('layoutApplied', true);
@@ -249,7 +533,7 @@ delegate(Templates.prototype, {
 
   mergePartials: function (locals, viewTypes) {
     var names = viewTypes || this.viewTypes.partial;
-    var opts = utils.extend({}, this.options, locals);
+    var opts = extend({}, this.options, locals);
 
     return names.reduce(function (acc, name) {
       var collection = this.views[name];
@@ -273,18 +557,18 @@ delegate(Templates.prototype, {
    * Build the context for the given `view` and `locals`.
    *
    * @name .context
-   * @param  {Object} `view` Template object
+   * @param  {Object} `view` Templates object
    * @param  {Object} `locals`
    * @return {Object} The object to be passed to engines/views as context.
    */
 
   context: function (view, ctx, locals) {
     var obj = {};
-    utils.mixin(obj, ctx);
-    utils.mixin(obj, this.cache.data);
-    utils.mixin(obj, view.data);
-    utils.mixin(obj, view.locals);
-    utils.mixin(obj, locals);
+    extend(obj, ctx);
+    extend(obj, this.cache.data);
+    extend(obj, view.data);
+    extend(obj, view.locals);
+    extend(obj, locals);
     return obj;
   },
 
@@ -294,15 +578,15 @@ delegate(Templates.prototype, {
 
   bindHelpers: function (view, locals, context, isAsync) {
     var helpers = {};
-    utils.extend(helpers, this.options.helpers);
-    utils.extend(helpers, this._.helpers.sync);
+    extend(helpers, this.options.helpers);
+    extend(helpers, this._.helpers.sync);
 
-    if (isAsync) utils.extend(helpers, this._.helpers.async);
-    utils.extend(helpers, locals.helpers);
+    if (isAsync) extend(helpers, this._.helpers.async);
+    extend(helpers, locals.helpers);
 
     // build the context to expose as `this` in helpers
     var thisArg = {};
-    thisArg.options = utils.extend({}, this.options, locals);
+    thisArg.options = extend({}, this.options, locals);
     thisArg.context = context || {};
     thisArg.context.view = view;
     thisArg.app = this;
@@ -322,7 +606,7 @@ delegate(Templates.prototype, {
   },
 
   /**
-   * Add default Router handlers to Template.
+   * Add default Router handlers to Templates.
    */
 
   handlers: function (methods) {
@@ -343,7 +627,7 @@ delegate(Templates.prototype, {
    */
 
   error: function(method, id, msg) {
-    return new Error(this.errors[method][id] + 'Template#' + method + ' ' + msg);
+    return new Error(this.errors[method][id] + 'Templates#' + method + ' ' + msg);
   }
 });
 
