c3f1b324d4091553eadb75929a0e136ac90440c0
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-08 16:59:39 -0400

    view methods tests

---
+++ test/view.methods.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* deps: mocha */
+var assert = require('assert');
+var should = require('should');
+var App = require('..');
+var app;
+
+describe('view.option()', function () {
+  beforeEach(function () {
+    app = new App();
+    app.engine('tmpl', require('engine-base'));
+    app.create('page');
+  });
+
+
+  describe('.use', function () {
+    it('should expose `.use` for running plugins on a view:', function () {
+      app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>'})
+        .use(function () {
+          this.options.foo = 'bar';
+        })
+        .use(function () {
+          this.options.bar = 'baz';
+        })
+
+      var page = app.pages.getView('a.tmpl');
+      page.options.should.have.property('foo');
+      page.options.should.have.property('bar');
+    });
+  });
+
+  describe('.render:', function (done) {
+    it('should expose `.render` for rendering a view:', function (done) {
+      app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}})
+        .render({}, function (err, res) {
+          if (err) return done(err);
+          res.contents.toString().should.equal('bbb');
+          done();
+        });
+    });
+  });
+});
