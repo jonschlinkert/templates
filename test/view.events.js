a8ada2bb13209a57702e68a70f9651ccd60f974f
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-08 16:55:14 -0400

    view.events tests

---
+++ test/view.events.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* deps: mocha */
+var assert = require('assert');
+var should = require('should');
+var App = require('..');
+var app;
+
+describe('view.option()', function () {
+  beforeEach(function () {
+    app = new App();
+    app.engine('tmpl', require('engine-base'));
+    app.create('page');
+  })
+
+  it('should emit events:', function () {
+    app.pages('a.tmpl', {path: 'a.tmpl', content: '<%= a %>'});
+    var page = app.pages.getView('a.tmpl');
+    var events = [];
+
+    page.on('option', function (key, value) {
+      events.push(key);
+    });
+
+    page.option('a', 'b');
+    page.option('c', 'd');
+    page.option('e', 'f');
+    page.option({g: 'h'});
+
+    events.should.eql(['a', 'c', 'e', 'g']);
+  });
+});
