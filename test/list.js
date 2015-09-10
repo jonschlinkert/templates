49f35d5f2c34e92b6a1bf8cedd76df3e9fe2ed72
Brian Woodward <brian.woodward@gmail.com>
2015-09-10 00:32:53 -0400

    adding list pagination

--- test/list.js
+++ test/list.js
@@ -351,6 +351,45 @@ describe('list', function () {
     });
   });
 
+  describe('paginate', function() {
+    var items = [
+      { path: 'a.md', locals: { date: '2014-01-01', foo: 'zzz', bar: 1 } },
+      { path: 'f.md', locals: { date: '2014-01-01', foo: 'mmm', bar: 2 } },
+      { path: 'd.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 3 } },
+      { path: 'i.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 5 } },
+      { path: 'k.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 1 } },
+      { path: 'j.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 4 } },
+      { path: 'h.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 6 } },
+      { path: 'l.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 7 } },
+      { path: 'e.md', locals: { date: '2015-01-02', foo: 'aaa', bar: 8 } },
+      { path: 'b.md', locals: { date: '2012-01-02', foo: 'ccc', bar: 9 } },
+      { path: 'f.md', locals: { date: '2014-06-01', foo: 'rrr', bar: 10 } },
+      { path: 'c.md', locals: { date: '2015-04-12', foo: 'ttt', bar: 11 } },
+      { path: 'g.md', locals: { date: '2014-02-02', foo: 'yyy', bar: 12 } },
+    ];
+
+    it('should paginate a list:', function () {
+      list = new List();
+      list.addList(items);
+
+      var res = list.paginate();
+      assert.equal(res.length, 2);
+      assert.containEql(res[0].items, items.slice(0, 10));
+      assert.containEql(res[1].items, items.slice(10));
+    });
+
+    it('should paginate a list with given options:', function () {
+      list = new List();
+      list.addList(items);
+
+      var res = list.paginate({limit: 5});
+      assert.equal(res.length, 3);
+      assert.containEql(res[0].items, items.slice(0, 5));
+      assert.containEql(res[1].items, items.slice(5, 10));
+      assert.containEql(res[2].items, items.slice(10));
+    });
+  });
+
   describe('Views instance', function() {
     beforeEach(function() {
       views = new Views();
