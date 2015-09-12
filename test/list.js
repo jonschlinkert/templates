033ba3f863d85ce392b1a9d73d34ca72b1315502
Brian Woodward <brian.woodward@gmail.com>
2015-09-12 17:25:55 -0400

    adding pagination method to List

--- test/list.js
+++ test/list.js
@@ -390,6 +390,35 @@ describe('list', function () {
     });
   });
 
+  describe('pagination', function() {
+    var items = [
+      { path: 'a.md', locals: { date: '2014-01-01', foo: 'zzz', bar: 1 } },
+      { path: 'f.md', locals: { date: '2014-01-01', foo: 'mmm', bar: 2 } },
+      { path: 'd.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 3 } },
+      { path: 'i.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 5 } },
+      { path: 'k.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 1 } },
+      { path: 'j.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 4 } },
+      { path: 'h.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 6 } },
+      { path: 'l.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 7 } },
+      { path: 'e.md', locals: { date: '2015-01-02', foo: 'aaa', bar: 8 } },
+      { path: 'b.md', locals: { date: '2012-01-02', foo: 'ccc', bar: 9 } },
+      { path: 'f.md', locals: { date: '2014-06-01', foo: 'rrr', bar: 10 } },
+      { path: 'c.md', locals: { date: '2015-04-12', foo: 'ttt', bar: 11 } },
+      { path: 'g.md', locals: { date: '2014-02-02', foo: 'yyy', bar: 12 } },
+    ];
+
+    it('should create a pagination array from the list:', function () {
+      list = new List();
+      list.addList(items);
+
+      var res = list.pagination();
+      assert.equal(res.length, items.length);
+      res.forEach(function (info) {
+        assert.containEql(info.item, items[info.idx]);
+      });
+    });
+  });
+
   describe('Views instance', function() {
     beforeEach(function() {
       views = new Views();
