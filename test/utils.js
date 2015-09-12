aac1044ffaf1b66d916b60bd9e4ae2443632081a
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-12 14:10:58 -0400

    utils test coverage

--- test/utils.js
+++ test/utils.js
@@ -52,49 +52,10 @@ describe('utils', function () {
     });
   });
 
-  describe('resolveGlob', function() {
-    it('should resolve absolute paths for a glob of files:', function () {
-      var files = utils.resolveGlob('test/fixtures/**/*.js');
-      assert(files.length > 0);
-      files.forEach(function (fp) {
-        assert(isAbsolute(fp));
-      });
-    });
-  });
-
-  describe('matchKey', function() {
-    it('should return null if the first argument is invalid:', function () {
-      assert(utils.matchKey('foo') === null);
-    });
-
-    it('should return the first property that matches the given glob:', function () {
-      var obj = {aaa: 'bbb', ccc: 'ddd'};
-      assert(utils.matchKey(obj, '*a') === 'bbb');
-    });
-  });
-
-  describe('matchKey', function() {
-    it('should return null if the first argument is invalid:', function () {
-      assert(utils.matchKey('foo') === null);
-    });
-  });
-
   describe('error', function() {
     it('should format an error message:', function () {
       var err = utils.error('foo: ', {a: 'b'});
       assert(err.message === 'foo: {"a":"b"}');
-    });
-  });
-
-  describe('tryRequire', function() {
-    it('should require a file:', function () {
-      var file = utils.tryRequire('test/fixtures/helpers/a.js');
-      assert(typeof file === 'function');
-    });
-
-    it('should return `null` when unsuccessful:', function () {
-      var file = utils.tryRequire('test/fixtures/helpers/fofofo.js');
-      assert(file === null);
     });
   });
 
@@ -111,20 +72,74 @@ describe('utils', function () {
     });
   });
 
-  describe('stripDot', function() {
-    it('should strip the dot preceding a file extension:', function () {
-      assert(utils.stripDot('.js') === 'js');
-      assert(utils.stripDot('js') === 'js');
+  describe('getLocals', function() {
+    it('should get locals from an object:', function () {
+      var a = {foo: 'bar', hash: {one: 'two'}};
+      var b = {baz: 'qux', hash: {three: 'four'}};
+
+      assert.deepEqual(utils.getLocals(a, b), {
+        three: 'four',
+        one: 'two',
+        baz: 'qux',
+        foo: 'bar'
+      });
+    });
+  });
+
+  describe('has', function() {
+    it('should return true if an array has the given element:', function () {
+      assert(utils.has(['a', 'b', 'c'], 'c'));
+      assert(!utils.has(['a', 'b', 'c'], 'd'));
     });
 
-    it('should throw an error when not a string:', function () {
-      (function () {
-        utils.stripDot();
-      }).should.throw('utils.stripDot() expects `ext` to be a string.');
+    it('should return true if an object has the given key:', function () {
+      assert(utils.has({a: 'b', c: 'd'}, 'a'));
+      assert(!utils.has({a: 'b', c: 'd'}, 'd'));
+    });
+  });
+
+  describe('hasAny', function() {
+    it('should return true if an array has any of the given elements:', function () {
+      assert(utils.hasAny(['a', 'b', 'c'], ['c', 'f']));
+      assert(!utils.hasAny(['a', 'b', 'c'], ['d', 'f']));
+    });
+
+    it('should return true if an object has any of the given keys:', function () {
+      assert(utils.hasAny({a: 'b', c: 'd'}, ['a', 'b']));
+      assert(!utils.hasAny({a: 'b', c: 'd'}, ['d', 'e']));
+    });
+  });
+
+  describe('isView', function() {
+    it('should return true if a value looks like a view:', function () {
+      assert(utils.isView({a: 'b', c: 'd', contents: '...'}));
+      assert(utils.isView({a: 'b', c: 'd', content: '...'}));
+      assert(utils.isView({a: 'b', c: 'd', path: '...'}));
+      assert(!utils.isView({a: 'b', c: 'd'}));
+    });
+  });
+
+  describe('matchKey', function() {
+    it('should return null if the first argument is invalid:', function () {
+      assert(utils.matchKey('foo') === null);
+    });
+
+    it('should return the first property that matches the given glob:', function () {
+      var obj = {aaa: 'bbb', ccc: 'ddd'};
+      assert(utils.matchKey(obj, '*a') === 'bbb');
+    });
+
+    it('should return null if the first argument is invalid:', function () {
+      assert(utils.matchKey('foo') === null);
     });
   });
 
   describe('requireGlob', function() {
+    it('should return an empty object for non-requireable files:', function () {
+      var files = utils.requireGlob('test/fixtures/**/*.txt');
+      assert.deepEqual(files, {});
+    });
+
     it('should return an object for a glob of files:', function () {
       var files = utils.requireGlob('test/fixtures/**/*.js');
       assert(files && typeof files === 'object');
@@ -152,6 +167,43 @@ describe('utils', function () {
         var val = files[key];
         assert(typeof val === 'function' || typeof val === 'object');
       });
+    });
+  });
+
+  describe('resolveGlob', function() {
+    it('should resolve absolute paths for a glob of files:', function () {
+      var files = utils.resolveGlob('test/fixtures/**/*.js');
+      assert(files.length > 0);
+      files.forEach(function (fp) {
+        assert(isAbsolute(fp));
+      });
+    });
+  });
+
+
+  describe('stripDot', function() {
+    it('should strip the dot preceding a file extension:', function () {
+      assert(utils.stripDot('.js') === 'js');
+      assert(utils.stripDot('js') === 'js');
+    });
+
+    it('should throw an error when not a string:', function () {
+      (function () {
+        utils.stripDot();
+      }).should.throw('utils.stripDot() expects `ext` to be a string.');
+    });
+  });
+
+
+  describe('tryRequire', function() {
+    it('should require a file:', function () {
+      var file = utils.tryRequire('test/fixtures/helpers/a.js');
+      assert(typeof file === 'function');
+    });
+
+    it('should return `null` when unsuccessful:', function () {
+      var file = utils.tryRequire('test/fixtures/helpers/fofofo.js');
+      assert(file === null);
     });
   });
 });
