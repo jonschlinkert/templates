fc6c7b44c96565769be046d0bcb4d11f42c6d1fb
Brian Woodward <brian.woodward@gmail.com>
2015-09-14 19:05:01 -0400

    another view.dest test

--- test/view.methods.js
+++ test/view.methods.js
@@ -82,12 +82,29 @@ describe('view.option()', function () {
 
     it('should allow passing a function to dest and setting a dest path as a string', function (done) {
       var view = app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}});
-      view.path = 'banana.html';
+      view.dest = 'banana.html';
       view.dest(function (file) {
         return 'test/actual';
       }, function (err, res) {
         if (err) return done(err);
         res.should.equal(path.resolve('test/actual/banana.html'));
+        done();
+      });
+    });
+
+    it('should allow setting a function on dest to override the default functionality', function (done) {
+      var view = app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}});
+      view.dest = function (dir, opts, cb) {
+        if (typeof opts === 'function') {
+          cb = opts;
+          opts = {};
+        }
+        cb(null, path.resolve(path.join(dir, this.path)));
+      };
+
+      view.dest('test/actual', function (err, res) {
+        if (err) return done(err);
+        res.should.equal(path.resolve('test/actual/a.tmpl'));
         done();
       });
     });
