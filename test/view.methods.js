24c5cd7ce3adb80afda5960b50e120bbdd58c657
Brian Woodward <brian.woodward@gmail.com>
2015-09-14 19:01:19 -0400

    tests for view.dest

--- test/view.methods.js
+++ test/view.methods.js
@@ -1,4 +1,5 @@
 require('should');
+var path = require('path');
 var App = require('..');
 var app;
 
@@ -34,6 +35,61 @@ describe('view.option()', function () {
           res.contents.toString().should.equal('bbb');
           done();
         });
+    });
+  });
+
+  describe('.dest:', function () {
+    it('should expose `.dest` for calculating the view destination:', function (done) {
+      app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}})
+        .dest('test/actual', function (err, res) {
+          if (err) return done(err);
+          res.should.equal(path.resolve('test/actual/a.tmpl'));
+          done();
+        });
+    });
+
+    it('should allow setting a dest path as a string', function (done) {
+      var view = app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}});
+      view.dest = 'apple.html';
+      view.dest('test/actual', function (err, res) {
+        if (err) return done(err);
+        res.should.equal(path.resolve('test/actual/apple.html'));
+        done();
+      });
+    });
+
+    it('should allow passing options', function (done) {
+      var view = app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}});
+      view.dest = 'apple.html';
+      view.dest('actual', {cwd: 'test'}, function (err, res) {
+        if (err) return done(err);
+        res.should.equal(path.resolve('test/actual/apple.html'));
+        done();
+      });
+    });
+
+    it('should allow passing a function to dest', function (done) {
+      var view = app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}});
+      view.dest(function (file) {
+        file.path = 'banana.html';
+        return 'test/actual';
+      }, function (err, res) {
+        if (err) return done(err);
+        res.should.equal(path.resolve('test/actual/banana.html'));
+        done();
+      });
+    });
+
+    it('should allow passing a function to dest and setting a dest path as a string', function (done) {
+      var view = app.page('a.tmpl', {path: 'a.tmpl', content: '<%= a %>', locals: {a: 'bbb'}});
+      view.path = 'banana.html';
+      view.dest(function (file) {
+        return 'test/actual';
+      }, function (err, res) {
+        if (err) return done(err);
+        res.should.equal(path.resolve('test/actual/banana.html'));
+        done();
+      });
     });
   });
 });
