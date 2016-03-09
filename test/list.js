328d58827c067f50383242af5a322540d26a98c2
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-09 16:59:58 -0500

    expose `groupBy` on `views`

--- test/list.js
+++ test/list.js
@@ -474,8 +474,8 @@ describe('list', function() {
       { path: 'h.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 6 } },
       { path: 'l.md', locals: { date: '2014-01-01', foo: 'xxx', bar: 7 } },
       { path: 'e.md', locals: { date: '2015-01-02', foo: 'aaa', bar: 8 } },
-      { path: 'b.md', locals: { date: '2012-01-02', foo: 'ccc', bar: 9 } },
-      { path: 'f.md', locals: { date: '2014-06-01', foo: 'rrr', bar: 10 } },
+      { path: 'm.md', locals: { date: '2012-01-02', foo: 'ccc', bar: 9 } },
+      { path: 'b.md', locals: { date: '2014-06-01', foo: 'rrr', bar: 10 } },
       { path: 'c.md', locals: { date: '2015-04-12', foo: 'ttt', bar: 11 } },
       { path: 'g.md', locals: { date: '2014-02-02', foo: 'yyy', bar: 12 } }
     ];
@@ -485,6 +485,17 @@ describe('list', function() {
       list.addList(items);
 
       var res = list.groupBy('locals.foo');
+      console.log(res)
+      var keys = ['zzz', 'mmm', 'xxx', 'aaa', 'ccc', 'rrr', 'ttt', 'yyy'];
+      assert.deepEqual(Object.keys(res), keys);
+    });
+
+    it('should group a collection by a property', function() {
+      list = new List();
+      list.addList(items);
+
+      var views = new Views(list);
+      var res = views.groupBy('locals.foo');
       var keys = ['zzz', 'mmm', 'xxx', 'aaa', 'ccc', 'rrr', 'ttt', 'yyy'];
       assert.deepEqual(Object.keys(res), keys);
     });
